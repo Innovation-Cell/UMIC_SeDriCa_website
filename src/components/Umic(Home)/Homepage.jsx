c2dded1ef4dcbd5e11670896175478aff35bfe52
IronFist396 <atharvroday396@gmail.com>
2024-10-09 12:35:48 +0530

    final animations

--- src/components/Umic(Home)/Homepage.jsx
+++ src/components/Umic(Home)/Homepage.jsx
@@ -5,12 +5,12 @@ import Content from "./Content";
 import Hero from "./Hero";
 import SedLoader from '../Preloader/SedLoader.jsx';
 import intro from '../../assets/Intro.gif'
-
+import img from '../../assets/Intro1.gif'
 
 function Homepage(){
     return(
         <>
-        <SedLoader gifUrl={intro} duration={6500}/>
+        <SedLoader gifUrl={img} duration={6100}/>
         <MyClass />
         <Logo/>
         <Hero/>
